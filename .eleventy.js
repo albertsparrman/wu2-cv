5a45cd1b54f2f8b2a82e3721f285aee9cd3fbaff
albertsparrman <albert.sparrman@elev.ga.ntig.se>
2022-10-07 11:06:26 +0200

    Jag älskar detta häringa

---
+++ .eleventy.js
@@ -0,0 +1,41 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    outputDir: './dist/img/'
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/css");
+    //eleventyConfig.addPassthroughCopy("src/img");
+    
+    //eleventyConfig.addShortcode('img', function (src, alt, width){
+    //    return `<img src="${src}" alt="${alt}" width="${width}" />`;
+    //});
+
+    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+    eleventyConfig.addLiquidShortcode("image", imageShortcode);
+    eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+
+    
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    }
+};
+
